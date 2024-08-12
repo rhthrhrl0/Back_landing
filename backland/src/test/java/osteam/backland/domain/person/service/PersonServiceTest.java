8e6c2034a9dba60332d04e528f802a7c1806e3d1
mendel <rhthrhrl0@naver.com>
2024-08-13 00:11:40 +0900

    test: 이름 기반 조회 결과 테스트 케이스 추가

--- backland/src/test/java/osteam/backland/domain/person/service/PersonServiceTest.java
+++ backland/src/test/java/osteam/backland/domain/person/service/PersonServiceTest.java
@@ -1,5 +1,6 @@
 package osteam.backland.domain.person.service;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 import osteam.backland.domain.person.entity.dto.PersonDTO;
+import osteam.backland.domain.person.entity.dto.search.SearchByNameDTO;
 import osteam.backland.domain.person.entity.dto.search.SearchByPhoneDTO;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -23,18 +25,38 @@ class PersonServiceTest {
     @Autowired
     private PersonSearchService personSearchService;
 
+    @BeforeEach
+    public void before() {
+        PersonDTO person1 = PersonDTO.builder()
+                .name("홍길동")
+                .phone("010-1111-2222")
+                .build();
+        PersonDTO person2 = PersonDTO.builder()
+                .name("홍길동")
+                .phone("010-3333-4444")
+                .build();
+        PersonDTO person3 = PersonDTO.builder()
+                .name("김철수")
+                .phone("010-5555-6666")
+                .build();
+
+        personCreateService.createAll(person1);
+        personCreateService.createAll(person2);
+        personCreateService.createAll(person3);
+    }
+
     @Test
     @DisplayName("입력 인풋으로 들어온 휴대폰 번호가 처음인 경우 데이터베이스에 해당 이름으로 추가됨")
     void createPhone() {
         // given
         PersonDTO personDTO = PersonDTO.builder()
                 .name("mendel")
-                .phone("01012341234")
+                .phone("010-1234-1234")
                 .build();
 
         // when
         personCreateService.createAll(personDTO);
-        SearchByPhoneDTO searchDTO = personSearchService.searchAllByPhone("01012341234");
+        SearchByPhoneDTO searchDTO = personSearchService.searchAllByPhone("010-1234-1234");
 
         // then
         assertAll(
@@ -50,20 +72,20 @@ class PersonServiceTest {
         // given
         PersonDTO firstPersonDTO = PersonDTO.builder()
                 .name("mendel")
-                .phone("01012341234")
+                .phone("010-1234-1234")
                 .build();
 
         PersonDTO secondPersonDTO = PersonDTO.builder()
                 .name("myeongjin")
-                .phone("01012341234")
+                .phone("010-1234-1234")
                 .build();
 
         // when
         personCreateService.createAll(firstPersonDTO);
-        SearchByPhoneDTO firstSearchDTO = personSearchService.searchAllByPhone("01012341234");
+        SearchByPhoneDTO firstSearchDTO = personSearchService.searchAllByPhone("010-1234-1234");
 
         personCreateService.createAll(secondPersonDTO);
-        SearchByPhoneDTO secondSearchDTO = personSearchService.searchAllByPhone("01012341234");
+        SearchByPhoneDTO secondSearchDTO = personSearchService.searchAllByPhone("010-1234-1234");
 
         // then
         assertAll(
@@ -77,6 +99,45 @@ class PersonServiceTest {
                         () -> assertThat(secondSearchDTO.getPersonOneToOne().getName()).isEqualTo("myeongjin"),
                         () -> assertThat(secondSearchDTO.getPersonOneToMany().getName()).isEqualTo("myeongjin")
                 )
+        );
+    }
+
+    @Test
+    @DisplayName("이름이 홍길동인 유저 모두 조회시 결과 2")
+    void searchPersonName_홍길동() {
+        // when
+        SearchByNameDTO searchByNameDTO = personSearchService.searchAllByName("홍길동");
+
+        assertAll(
+                () -> assertThat(searchByNameDTO.getPersonOnly().size()).isEqualTo(2),
+                () -> assertThat(searchByNameDTO.getPersonOneToOne().size()).isEqualTo(2),
+                () -> assertThat(searchByNameDTO.getPersonOneToMany().size()).isEqualTo(2)
+        );
+    }
+
+    @Test
+    @DisplayName("이름이 김철수인 유저 모두 조회시 결과 1")
+    void searchPersonName_김철수() {
+        // when
+        SearchByNameDTO searchByNameDTO = personSearchService.searchAllByName("김철수");
+
+        assertAll(
+                () -> assertThat(searchByNameDTO.getPersonOnly().size()).isEqualTo(1),
+                () -> assertThat(searchByNameDTO.getPersonOneToOne().size()).isEqualTo(1),
+                () -> assertThat(searchByNameDTO.getPersonOneToMany().size()).isEqualTo(1)
+        );
+    }
+
+    @Test
+    @DisplayName("이름이 이몽룡인 유저 모두 조회시 결과 0")
+    void searchPersonName_이몽룡() {
+        // when
+        SearchByNameDTO searchByNameDTO = personSearchService.searchAllByName("이몽룡");
+
+        assertAll(
+                () -> assertThat(searchByNameDTO.getPersonOnly().size()).isEqualTo(0),
+                () -> assertThat(searchByNameDTO.getPersonOneToOne().size()).isEqualTo(0),
+                () -> assertThat(searchByNameDTO.getPersonOneToMany().size()).isEqualTo(0)
         );
     }
 }
